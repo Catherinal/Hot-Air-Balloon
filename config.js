1331136531f4eafa09bc7633dde81f957e159b54
Catherinal <75525803+Catherinal@users.noreply.github.com>
2022-01-18 17:02:46 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEAhGjJDdWxMsBVd41NuW9h4nE2oCFXT8",
+    authDomain: "hot-air-balloon-4fb13.firebaseapp.com",
+    databaseURL: "https://hot-air-balloon-4fb13-default-rtdb.firebaseio.com",
+    projectId: "hot-air-balloon-4fb13",
+    storageBucket: "hot-air-balloon-4fb13.appspot.com",
+    messagingSenderId: "688385028996",
+    appId: "1:688385028996:web:82a6a49a22feb13bf323d1"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);  
